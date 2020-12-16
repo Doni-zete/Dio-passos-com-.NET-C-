5457f66b1498385cbee9d9ad034f0f88b4671bb2
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-15 22:51:34 -0300

    instruçoes

--- 02INSTRUÇÃO/Program.cs
+++ 02INSTRUÇÃO/Program.cs
@@ -29,8 +29,68 @@ namespace Instrucoes
                 Console.WriteLine($"{args.Length}argumentos");
             }
         }
-    }
 
-}
+        static void InstrucoesSwitch(string[] args)
+        {
+            int numeroDeArgumentos = args.Length;
+            switch (numeroDeArgumentos)
+            {
+                case 0:
+                    Console.WriteLine("Nenhum argumento");
+                    break;
+                case 1:
+                    Console.WriteLine("Um argumento");
+                    break;
+                default:
+                    Console.WriteLine($"{numeroDeArgumentos} argumentos");
+                    break;
+
+            }
+        }
+
+
+        static void InstrucoesWhile(string[] args)
+        {
+            int i = 0;
+            while (i < args.Length)
+            {
+                Console.WriteLine(args[i]);
+                i++;
+            }
+        }
+
+        static void InstrucoesDo(string[] args)
+        {
+            string texto;
+            do
+            {
+                TimeoutException = Console.ReadLine();
+                Console.WriteLine(texto);
+            } while (!string.IsNullOrEmpy(texto));
+        }
+
+
+        static void InstrucoesFor(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+
+
+        static void InstrucoesForeach(string[] args)
+        {
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+
+
+
+    }
 
 }

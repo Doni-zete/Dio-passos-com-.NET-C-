bcb02efd80654b75dbb1e25b69f1b94614401aef
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-16 22:38:10 -0300

    Intruções pontos

--- 03INSTRUÇÃO/01-Herança/Ponto.cs
+++ 03INSTRUÇÃO/01-Herança/Ponto.cs
@@ -1,6 +1,25 @@
 namespace Classes.Herança
 {
-
     public class Ponto
-    
+    {
+
+        public int _x, _y;
+
+        private int distancia;
+
+        public Ponto(int x, int y)
+
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        protected void CalcularDistancia()
+        {
+                //Faz alguma coisa
+        }
+
+        private void CalcularDistancia2()
+    }
+
 }

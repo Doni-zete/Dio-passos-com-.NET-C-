f6c59ccbc5803cdb5650745787af51f309559e60
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-18 21:30:09 -0300

    resolução

--- EXERCICIOS/Introdução a Programação com C#/2 - Distância/program.cs
+++ EXERCICIOS/Introdução a Programação com C#/2 - Distância/program.cs
@@ -18,6 +18,6 @@ class Desafio {
     static void Main() {
         int quilometros = Int32.Parse(Console.ReadLine());
         int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
-        Console.WriteLine(minutos + " minutos");
+        Console.WriteLine(minutos + "minutos");
     }
 }

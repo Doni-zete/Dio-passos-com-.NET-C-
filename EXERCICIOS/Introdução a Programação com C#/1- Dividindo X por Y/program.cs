85e360dae373b466546f3700ef9885d6f135828d
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-18 21:30:46 -0300

    resolução

--- EXERCICIOS/Introdução a Programação com C#/1- Dividindo X por Y/program.cs
+++ EXERCICIOS/Introdução a Programação com C#/1- Dividindo X por Y/program.cs
@@ -19,7 +19,7 @@ namespace program
             int limit = Int32.Parse(Console.ReadLine());
             for (int i = 0; i < limit; i++)
             {
-                string[] line = Console.ReadLine().Split(" ");
+                string[] line = Console.ReadLine().Split("  ");
                 double X = double.Parse(line[0]);
                 double Y = double.Parse(line[1]);
                 if (Y == 0)

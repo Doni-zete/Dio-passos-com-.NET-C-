c88a57793b306c4fb902d85422134a9f4deaab94
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-26 21:18:38 -0300

    solução

--- EXERCICIOS/Introdução a Programação com C#/2 - Distância/program.cs
+++ EXERCICIOS/Introdução a Programação com C#/2 - Distância/program.cs
@@ -13,11 +13,28 @@ Imprima o tempo necessário para a moto Y ficar com a quantidade K de quilômetr
 
 
 using System;
-
-class Desafio {
-    static void Main() {
-        int quilometros = Int32.Parse(Console.ReadLine());
-        int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
-        Console.WriteLine(minutos + "minutos");
+namespace program
+{
+    class program
+    {
+        static void Main()
+        {
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++)
+            {
+                string[] line = Console.ReadLine().Split("  ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double divisao = X / Y; // Digite aqui o calculo da divisao
+                    Console.WriteLine(divisao.ToString("N1"));
+                }
+            }
+        }
     }
 }
